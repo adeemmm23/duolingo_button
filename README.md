757416c3d0f52f27b05ce95416343d160ffd9ad9
Adem Othman <contact.adem.ot@gmail.com>
2023-07-29 14:55:00 +0100

    Moved to random activity using API

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# duolingo_button
-Just playing around 
+# Duolingo Button
+## Description
+This is a simple script that adds a button to the Duolingo website that allows you to play a sound of the word you are hovering over. It uses the Google Translate API to get the sound file. It also adds a button to the top of the page that allows you to play the sound of the word you are currently learning.
--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// random words list
-randomWords = ['Hello', 'World', 'JavaScript', 'HTML', 'CSS', 'Web', 'Programming', 'Computer', 'Science', 'Engineer', 'Software', 'Developer']
-
-// function to be called when button is clicked
 clicked = function(){
-    // generate random number
-    randomNum = Math.floor(Math.random() * 12)
-    // append random word to random id
-    random = document.getElementById('random').innerHTML = randomWords[randomNum]
+    fetch("https://www.boredapi.com/api/activity/")
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('random').innerHTML = data.activity;
+    })
+    .catch(err => console.log(err))
 }
