e9c4a56338999cc4548e0568990eeef998fa5884
Adem Othman <contact.adem.ot@gmail.com>
2023-07-29 15:29:06 +0100

    Made a machine writer animation

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-clicked = function(){
-    fetch("https://www.boredapi.com/api/activity/")
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById('random').innerHTML = data.activity;
+let i = 0;
+clicked();
+
+function clicked() {
+  fetch("https://www.boredapi.com/api/activity/")
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById("random").innerHTML = "";
+      i = 0;
+      console.log(data);
+      typeWriter(data.activity);
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
+}
+
+function typeWriter(text) {
+    if (i < text.length) {
+        document.getElementById("random").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 50, text);
+    }
 }
