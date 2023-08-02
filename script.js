3c32dcc0c9788c7c2c1c03b0ae5a464e8967bb2e
Adem Othman <contact.adem.ot@gmail.com>
2023-08-02 14:19:20 +0100

    Deleted not needed code

--- script.js
+++ script.js
@@ -99,15 +99,6 @@ async function copyToClipboard() {
   lastCopied = boredList[boredList.length - 1];
 }
 
-// Copy function for mobiles
-async function copy(text){
-  var input = document.createElement('input');
-  input.setAttribute('value', text);
-  document.body.appendChild(input);
-  input.select();
-  var result = document.execCommand('copy');
-  document.body.removeChild(input);
-}
 
 async function playIcons() {
   let i = 0;
